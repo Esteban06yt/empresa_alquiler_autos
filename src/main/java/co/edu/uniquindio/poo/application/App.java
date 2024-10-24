e9ac42a668c6c1367b38806e082e3751683e606e
Esteban06yt <133072260+Esteban06yt@users.noreply.github.com>
2024-10-24 12:19:38 -0500

    Initial commit

---
+++ src/main/java/co/edu/uniquindio/poo/application/App.java
@@ -0,0 +1,7 @@
+package co.edu.uniquindio.poo.application;
+
+public class App {
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ src/main/java/co/edu/uniquindio/poo/model/Administrador.java
@@ -0,0 +1,8 @@
+package co.edu.uniquindio.poo.model;
+
+public class Administrador extends Persona{
+
+    public Administrador(String nombre, String apellido, String cedula, String telefono, String correo){
+        super(nombre, apellido, cedula, telefono, correo);
+    }
+}
